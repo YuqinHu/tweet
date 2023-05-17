983284a43f01bc97d0da0ee331ce6f484fd70597
YuqinHu <h878688736@gmail.com>
2023-05-16 20:36:08 -0700

    finish tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,64 +1,83 @@
 $(document).ready(function() {
-  const data = [
-      {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png"
-          ,
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-      },
-      {
-        "user": {
-          "name": "Descartes",
-          "avatars": "https://i.imgur.com/nlhLi3I.png",
-          "handle": "@rd" },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-      }
-  ]
-
-  const renderTweets = function(tweets) {
-    for (obj of data) {
-      const $dynamicTweet = createTweetElement(obj);
-      $('.tweets-container').prepend($dynamicTweet);
-    }
+  //preventing XSS with escaping    
+  const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
     
-
-  }
-
-  const createTweetElement = function(tweet) {
-    let $tweet = `
-    <article class = "tweet">
-    <header>
-      <div>
-        <img src="${tweet.user.avatars}">
-        &nbsp&nbsp${tweet.user.name}
-      </div>
-      <div>
-        <a>${tweet.user.handle}</a>
-      </div>
-    </header>
-    <body>
-      <p class="user-post">${tweet.content.text}</p>
-    </body>
-    <footer>
-      <div>${tweet.created_at}</div>
-      <div>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`
-    return $tweet;
-  }
-
-  renderTweets(data);
-})
+    const renderTweets = function(tweets) {
+      // loops through tweets
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      $(".tweets-container").empty();
+      for (obj of tweets) {
+        const $tweetElement = createTweetElement(obj);
+        $('.tweets-container').prepend($tweetElement);
+      }
+    }
+  
+  
+    //Fetching tweets with Ajax
+    const createTweetElement = function(tweet) {
+      let $tweet = `
+      <article class = "tweet">
+      <header>
+        <div>
+          <img src="${escape(tweet.user.avatars)}">
+          &nbsp&nbsp${escape(tweet.user.name)}
+        </div>
+        <div>
+          <a>${escape(tweet.user.handle)}</a>
+        </div>
+      </header>
+      <body>
+        <p class="user-post">${escape(tweet.content.text)}</p>
+      </body>
+      <footer>
+        <div>
+        ${timeago.format(tweet.created_at)}
+        </div>
+        <div>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+  
+    </article>`
+  
+      return $tweet;
+    };
+  
+  
+    $('.form').submit(function(event) {
+      event.preventDefault();
+      const tweetData = $('#tweet-text').val();
+      if (tweetData.length === 0) {
+        $(".error").text("Please enter a message to tweet.");
+      } else if (tweetData.length > 140) {
+        $(".error").text("Your tweet is too long. Please shorten your message to 140 characters.");
+      } else {
+        const serializeTweet = $(this).serialize();
+        $.post('http://localhost:8080/tweets', serializeTweet, (result) => {
+          $("textarea").val("");
+          $(".counter").text(140);
+          loadTweets();
+        });
+      }
+    });
+  
+    const loadTweets = () => {
+      $.get('http://localhost:8080/tweets', (data) => {
+        renderTweets(data);
+      });
+    };
+  
+    loadTweets();
+    });
+  
+  
+  
+  
+  
