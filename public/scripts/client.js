776c9153a7ebc7e6a67280677a1b2b7f0f133c63
YuqinHu <h878688736@gmail.com>
2023-05-16 17:33:42 -0700

    change in my style

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,47 +1,45 @@
 $(document).ready(function() {
   const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
+      {
+        "user": {
+          "name": "Newton",
+          "avatars": "https://i.imgur.com/73hZDYK.png"
+          ,
+          "handle": "@SirIsaac"
+        },
+        "content": {
+          "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
+      {
+        "user": {
+          "name": "Descartes",
+          "avatars": "https://i.imgur.com/nlhLi3I.png",
+          "handle": "@rd" },
+        "content": {
+          "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+      }
   ]
-  
+
   const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
     for (obj of data) {
-      const $tweetElement = createTweetElement(obj);
-      $('.tweets-container').prepend($tweetElement);
+      const $dynamicTweet = createTweetElement(obj);
+      $('.tweets-container').prepend($dynamicTweet);
     }
+    
+
   }
-  
-  
+
   const createTweetElement = function(tweet) {
     let $tweet = `
     <article class = "tweet">
     <header>
       <div>
         <img src="${tweet.user.avatars}">
-         &nbsp&nbsp${tweet.user.name}
+        &nbsp&nbsp${tweet.user.name}
       </div>
       <div>
         <a>${tweet.user.handle}</a>
@@ -59,9 +57,8 @@ $(document).ready(function() {
       </div>
     </footer>
   </article>`
-  
     return $tweet;
-  };
-  
+  }
+
   renderTweets(data);
-  });
+})
